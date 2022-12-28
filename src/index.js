f596620ab4d415fed83cbf9df91d884a88d06936
Rostislav Antonov <antonov.qwertylox004@yandex.ru>
2022-12-28 13:35:10 +0100

    add login page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import "./index.scss";
 import LogInPage from "./pages/LogInPage";
 import ChooseModePage from "./pages/ChooseModePage";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
