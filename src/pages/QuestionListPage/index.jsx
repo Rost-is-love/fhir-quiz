9ef63be4a1b775773b7e6f80994dbd9672fd1410
eremec <ilya.eremeev@health-samurai.io>
2022-12-28 18:24:09 +0200

    Add questions list page

--- src/pages/QuestionListPage/index.jsx
+++ src/pages/QuestionListPage/index.jsx
@@ -1,14 +1,35 @@
 import "../../App.scss";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 export default function QuestionListPage() {
+  const [questionsfull, setQuestion] = useState([]);
+  useEffect(() => {
+    console.log("Page opened; Json fetch here")
+    axios.get(`https://fhirquiz.edge.aidbox.app/Question`)
+      .then(res => {
+        setQuestion(res.data.entry);
+        console.log(typeof(questionsfull));
+        console.log(questionsfull);
+      })
+  });
+
   return (
     <div>
-      <header>
-        <h1>Questions</h1>
-      </header>
-      <div className="centered content">
-        <div className="question"> Question 1
+      <div className="centered content question-list">
+        <header>
+          <h1>Questions</h1>
+        </header>
+
+        <div className="grid">
+          {questionsfull.map((q, idx)=>{
+            return (<div className="question" key={idx}>
+                    <span> {q.resource.question} </span>
+                    <span> {q.resource.author.id} </span>
+                    </div>)
+          })}
         </div>
+
       </div>
     </div>
   );
