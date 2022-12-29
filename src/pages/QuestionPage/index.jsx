523eeb0059b19659e41ac39e5307a76e52dd39ce
Rostislav Antonov <antonov.qwertylox004@yandex.ru>
2022-12-29 13:48:22 +0100

    fix question page

--- src/pages/QuestionPage/index.jsx
+++ src/pages/QuestionPage/index.jsx
@@ -36,107 +36,81 @@ const submitResponse = async (
   });
   const selectedOption = document.getElementById(selectedElement);
   selectedOption.classList.remove("text-white");
-  console.log(selectedOption, "selectedOption");
+  // console.log(selectedOption, "selectedOption");
   if (userResponse.trim().toLowerCase() === rightAnswer.trim().toLowerCase()) {
     selectedOption.classList.add("text-green-500");
   } else {
     selectedOption.classList.add("text-red-500");
   }
-  console.log(questionResponse, "QuestionResponse");
+  // console.log(questionResponse, "QuestionResponse");
 };
 
 const removeLike = async (likeId) => {
+  console.log(likeId, "likeIdlikeId");
   await axios.delete(`https://fhirquiz.edge.aidbox.app/Like/${likeId}`);
-};
-
-const addLike = async (userId, questionId) => {
-  await axios.post(`https://fhirquiz.edge.aidbox.app/Like`, {
-    user: {
-      resourceType: "User",
-      id: userId,
-    },
-    question: {
-      resourceType: "Question",
-      id: questionId,
-    },
-  });
 };
 
 export default function QuestionPage() {
   const currentUser = useStore(user);
-  const [question, setQuestion] = useState({});
-  const [author, setAuthor] = useState({});
+  const [questionData, setQuestionData] = useState({});
   const [userResponse, setUserResponse] = useState("");
+  const [like, setLike] = useState(false);
   const [selectedElement, setSelectedElement] = useState("");
-  const [like, setLike] = useState({});
   const params = useParams();
+
   const questionId = params.id;
-
-  // Question
-  // QuestionResponse
-  // User as author
-  // Like
-
-  // params
-  // questionId
-  // currentUser.id
+  console.log(like, "clickLike");
+  console.log(questionData, "questionDataquestionData");
   useEffect(() => {
     async function fetchData() {
-
-
-      if (Object.keys(question).length == 0) {
-      const questionData = await axios.get(
-        `https://fhirquiz.edge.aidbox.app/Question/${questionId}`
+      const res = await axios.get(
+        `https://fhirquiz.edge.aidbox.app/$query/question-data?currentUserId=${currentUser.id}&questionId=${questionId}`
       );
-      setQuestion(questionData.data);
-      };
 
-      if (Object.keys(author).length == 0) {
-      const authorData = await axios.get(
-        `https://fhirquiz.edge.aidbox.app/User/${question.author.id}`
-      );
-      setAuthor(authorData.data);
-      };
-
-      if (userResponse.length == 0) {
-      const responseData = await axios.get(
-        `https://fhirquiz.edge.aidbox.app/QuestionResponse?.user.id=${currentUser.id}&.question.id=${question.id}&_sort=createdAt`
-      );
-      setUserResponse(responseData.data[0].response);
-      };
-
-      if (Object.keys(like).length == 0) {
-      const likeData = await axios.get(
-        `https://fhirquiz.edge.aidbox.app/Like?.user.id=${currentUser.id}&.question.id=${question.id}`
-      );
-      setLike(likeData.data);
-      };
-
+      setQuestionData(res.data?.data[0]?.resource);
+      if (res.data?.data[0]?.resource?.like?.id) {
+        setLike(true);
+      }
     }
     fetchData();
-  });
+  }, []);
+
+  const addLike = async () => {
+    const res = await axios.post(`https://fhirquiz.edge.aidbox.app/Like`, {
+      user: {
+        resourceType: "User",
+        id: currentUser.id,
+      },
+      question: {
+        resourceType: "Question",
+        id: questionData.id,
+      },
+    });
+
+    const likeId = res.data.id;
+    const newState = { ...questionData, like: { id: likeId } };
+
+    setQuestionData(newState);
+  };
 
   const likeStyles = classNames({
     "mt-6": true,
     "hover:cursor-pointer": true,
-    "text-white": !like.question,
-    "ext-pink-600": like.question,
+    "text-white": !like,
+    "text-pink-600": like,
   });
 
-  if (Object.keys(question).length == 0)
-  {return (<div></div>);}
-  else {
   return (
     <div className="first">
       <div className="first__container question-container">
         <section>
-          <div className="mb-10 flex justify-between">
-            <h1 className="text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-yellow-400 to-pink-600">
-              Question #{questionId.split("-")[1]}
+          <div className="mb-10 flex justify-start items-end">
+            <h1 className="text-3xl mr-3 font-bold text-transparent bg-clip-text bg-gradient-to-r from-yellow-400 to-pink-600">
+              Question #{questionId}
             </h1>
-            <h2 className="text-3xl font-semibold text-transparent bg-clip-text bg-gradient-to-r from-yellow-400 to-pink-600">
-              Author: {author.title}
-            </h2>
+            <span className="text-l pb-1 font-semibold text-white">
+              by {questionData?.user?.name?.formatted}
+            </span>
           </div>
           <div
             className="
@@ -147,13 +121,13 @@ export default function QuestionPage() {
             border-gray-300
             rounded-lg"
           >
-            <h2 className="text-white text-2xl font-semibold mb-10 whitespace-pre">
-              {question.question}
-            </h2>
+            <p className="text-white text-xl text-left font-semibold mb-10">
+              {questionData.question}
+            </p>
             <div className="flex gap-4 flex-col mb-7">
-              {question.options?.map(({ value }, i) => {
+              {questionData.options?.map(({ value }, i) => {
                 return (
-                  <div className="flex items-center mb-1">
+                  <div className="flex items-center mb-1 hover:cursor-pointer">
                     <input
                       id={`default-radio-${i + 1}`}
                       type="radio"
@@ -168,7 +142,7 @@ export default function QuestionPage() {
                     <label
                       id={`label-radio-${i + 1}`}
                       for={`default-radio-${i + 1}`}
-                      className="option-text ml-2 text-xl font-medium text-white"
+                      className="option-text ml-2 text-l text-left font-medium text-white"
                     >
                       {value}
                     </label>
@@ -182,8 +156,8 @@ export default function QuestionPage() {
                 onClick={() =>
                   submitResponse(
                     userResponse,
-                    question.answer,
-                    question.id,
+                    questionData.answer,
+                    questionData.id,
                     currentUser.id,
                     selectedElement
                   )
@@ -194,26 +168,29 @@ export default function QuestionPage() {
               <div
                 className={likeStyles}
                 onClick={() => {
-                  if (like.question) {
-                    removeLike(like.id);
-                    setLike({});
+                  if (questionData.like?.id) {
+                    removeLike(questionData.like?.id);
                   } else {
-                    addLike(currentUser.id, question.id);
+                    addLike(currentUser.id, questionData.id);
+                  }
+
+                  if (like) {
+                    setLike(false);
+                  } else {
+                    setLike(true);
                   }
                 }}
               >
                 <svg
-                  fill="#000000"
                   version="1.1"
                   id="Filled_Icons"
                   xmlns="http://www.w3.org/2000/svg"
                   x="0px"
                   y="0px"
                   viewBox="0 0 24 24"
-                  enable-background="new 0 0 24 24"
                   className="w-10 h-10 fill-current mr-5"
                 >
-                  <g id="Like-Filled">
+                  <g>
                     <path d="M23,10v10c0,1.66-1.34,3-3,3H6V11h2c1.66,0,3-1.24,3-2.9V1h2.1C14.76,1,16,2.34,16,4v6H23z M1,23h3V11H1V23z" />
                   </g>
                 </svg>
@@ -222,17 +199,24 @@ export default function QuestionPage() {
           </div>
           <div className="mt-10 rounded-xl bg-gradient-to-r from-yellow-400 to-pink-600">
             <a
-              href={`#/question/question-${
-                Number(questionId.split("-")[1]) + 1
-              }`}
+              href={`#/question/${Number(questionId) + 1}`}
               className="p-3 flex items-center rounded-xl text-2xl font-medium justify-center w-full bg-transparent hover:text-white duration-500"
             >
               <svg
+                className="w-10 h-10 fill-current mr-5"
+                viewBox="0 0 16 16"
                 xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 32 32"
-                className="w-5 h-5 fill-current mr-5"
+                fill="none"
               >
-                <path d="M16 0.396c-8.839 0-16 7.167-16 16 0 7.073 4.584 13.068 10.937 15.183 0.803 0.151 1.093-0.344 1.093-0.772 0-0.38-0.009-1.385-0.015-2.719-4.453 0.964-5.391-2.151-5.391-2.151-0.729-1.844-1.781-2.339-1.781-2.339-1.448-0.989 0.115-0.968 0.115-0.968 1.604 0.109 2.448 1.645 2.448 1.645 1.427 2.448 3.744 1.74 4.661 1.328 0.14-1.031 0.557-1.74 1.011-2.135-3.552-0.401-7.287-1.776-7.287-7.907 0-1.751 0.62-3.177 1.645-4.297-0.177-0.401-0.719-2.031 0.141-4.235 0 0 1.339-0.427 4.4 1.641 1.281-0.355 2.641-0.532 4-0.541 1.36 0.009 2.719 0.187 4 0.541 3.043-2.068 4.381-1.641 4.381-1.641 0.859 2.204 0.317 3.833 0.161 4.235 1.015 1.12 1.635 2.547 1.635 4.297 0 6.145-3.74 7.5-7.296 7.891 0.556 0.479 1.077 1.464 1.077 2.959 0 2.14-0.020 3.864-0.020 4.385 0 0.416 0.28 0.916 1.104 0.755 6.4-2.093 10.979-8.093 10.979-15.156 0-8.833-7.161-16-16-16z"></path>
+                <g>
+                  <path d="M8.245 4.695a.75.75 0 00-.05 1.06l1.36 1.495H4.75a.75.75 0 000 1.5h4.805l-1.36 1.495a.75.75 0 001.11 1.01l2.5-2.75a.75.75 0 000-1.01l-2.5-2.75a.75.75 0 00-1.06-.05z" />
+
+                  <path
+                    fill-rule="evenodd"
+                    d="M0 8a8 8 0 1116 0A8 8 0 010 8zm8-6.5a6.5 6.5 0 100 13 6.5 6.5 0 000-13z"
+                    clip-rule="evenodd"
+                  />
+                </g>
               </svg>
               Next Question
             </a>
@@ -240,5 +224,5 @@ export default function QuestionPage() {
         </section>
       </div>
     </div>
-  );}
+  );
 }
