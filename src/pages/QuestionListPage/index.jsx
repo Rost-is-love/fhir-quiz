d930af2f6339094d90e609d49c81ee7f639bd686
eremec <ilya.eremeev@health-samurai.io>
2022-12-29 12:40:17 +0200

    Remove mocking elements from questin list page

--- src/pages/QuestionListPage/index.jsx
+++ src/pages/QuestionListPage/index.jsx
@@ -7,10 +7,9 @@ export default function QuestionListPage() {
   useEffect(() => {
     console.log("Page opened; Json fetch here");
     if (questionsfull.length == 0) {
-      axios.get(`https://fhirquiz.edge.aidbox.app/Question`).then((res) => {
-        setQuestion(res.data.entry);
-        console.log(typeof questionsfull);
-        console.log(questionsfull);
+      axios.get(`https://fhirquiz.edge.aidbox.app/$query/questions`).then((res) => {
+        setQuestion(res.data.data);
+        //console.log(questionsfull);
       });
     }
   });
@@ -18,7 +17,7 @@ export default function QuestionListPage() {
   return (
     <div className="question-list">
       <div className="">
-        <div className="w-full p-6 lg:max-w-md z-50">
+        <div className="w-full p-6 z-50">
           <h1 className="text-3xl mb-7 font-semibold text-center text-transparent bg-clip-text bg-gradient-to-r from-yellow-400 to-pink-600">
             Pick your poison
           </h1>
@@ -30,25 +29,23 @@ export default function QuestionListPage() {
           <div className="grid">
             {questionsfull.map((q, idx) => {
               return (
-                <a href={"#/question/" + q.resource.id}>
+                <a key={idx} href={"#/question/" + q.resource_data.question.id}>
                   <div
                     className="question"
-                    key={idx}
-                    title={q.resource.question}
-                  >
+                    title={q.resource_data.question.resource.question}>
                     <div>
                       <div className="arrow-up bg-gradient-to-r from-yellow-400 to-pink-600">
                         {" "}
                       </div>
-                      <div className="text-center"> {idx} </div>
+                      <div className="text-center"> {q.likecount} </div>
                       <div className="arrow-down"> </div>
                     </div>
                     <span className="name ml-5">
-                      {q.resource.question.length > 50
-                        ? q.resource.question.substring(0, 50) + "..."
-                        : q.resource.question}
+                      {q.resource_data.question.resource.question.length > 50
+                        ? q.resource_data.question.resource.question.substring(0, 50) + "..."
+                        : q.resource_data.question.resource.question}
                     </span>
-                    <span className="italic mr-5"> by the best Author </span>
+                    <span className="italic mr-5"> {q.resource_data.user.resource.name.formatted} </span>
                   </div>
                 </a>
               );
