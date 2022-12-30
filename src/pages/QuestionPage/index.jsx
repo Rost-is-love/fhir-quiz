85fc731069dcec00d3fa80ae50672a2a8980f2cb
Rostislav Antonov <antonov.qwertylox004@yandex.ru>
2022-12-30 08:10:22 +0100

    switch to next not answered question

--- src/pages/QuestionPage/index.jsx
+++ src/pages/QuestionPage/index.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useStore } from "@nanostores/react";
-import classNames from "classnames";
 import axios from "axios";
 import "../../App.scss";
 import { user } from "../../store/user";
@@ -59,15 +58,37 @@ const removeLike = async (likeId) => {
   await axios.delete(`https://fhirquiz.edge.aidbox.app/Like/${likeId}`);
 };
 
-const getNextQuestion = (currentQuestion, allQuestions) => {
-  const currentQuestionIdx = allQuestions.findIndex(
+const getNextQuestion = (currentQuestion, allQuestions, userId) => {
+  const notAnsweredQuestions = allQuestions.filter((question) => {
+    const currentUserRes = question.q_resps?.find(
+      (res) => res?.user?.id === userId
+    );
+
+    return currentUserRes &&
+      currentUserRes?.response.trim() === question.q_answer.trim()
+      ? false
+      : true;
+  });
+
+  const missNextQuestion =
+    (notAnsweredQuestions.length === 1 &&
+      notAnsweredQuestions[0].q_id === currentQuestion.id) ||
+    notAnsweredQuestions.length === 0;
+
+  if (missNextQuestion) {
+    return false;
+  }
+
+  const currentQuestionIdx = notAnsweredQuestions.findIndex(
     (item) => item.q_id === currentQuestion.id
   );
 
   const nextQuestionInx =
-    currentQuestionIdx + 1 === allQuestions.length ? 0 : currentQuestionIdx + 1;
+    currentQuestionIdx + 1 === notAnsweredQuestions.length
+      ? 0
+      : currentQuestionIdx + 1;
 
-  return allQuestions[nextQuestionInx].q_id;
+  return notAnsweredQuestions[nextQuestionInx].q_id;
 };
 
 export default function QuestionPage() {
@@ -100,7 +121,8 @@ export default function QuestionPage() {
 
       const nextQeust = getNextQuestion(
         currentQuestion.data?.data[0]?.resource,
-        allQuestions.data.data
+        allQuestions.data.data,
+        currentUser.id
       );
       setNextQuestion(nextQeust);
     }
@@ -108,7 +130,7 @@ export default function QuestionPage() {
     if (currentUser) {
       fetchData();
     }
-  }, [questionId, currentUser]);
+  }, [questionId, currentUser, nextQuestion]);
 
   const addLike = async () => {
     const res = await axios.put(
@@ -130,13 +152,6 @@ export default function QuestionPage() {
 
     setQuestionData(newState);
   };
-
-  const likeStyles = classNames({
-    "mt-6": true,
-    "hover:cursor-pointer": true,
-    "text-white": !like,
-    "text-pink-600": like,
-  });
 
   if (questionData) {
     return (
@@ -313,11 +328,11 @@ export default function QuestionPage() {
           <p className="tip-text"></p>
         </div>
 
-        {nextQuestion && (
+        {nextQuestion ? (
           <div className="mt-10 rounded-xl bg-gradient-to-r from-yellow-400 to-pink-600 hover:from-pink-600 hover:to-yellow-400 hover:duration-500">
             <a
               href={`#/question/${nextQuestion}`}
-              className="p-3 flex items-center rounded-xl text-2xl font-medium justify-center w-full bg-transparent hover:text-white duration-500"
+              className="p-3 flex items-center rounded-xl text-2xl font-medium justify-center w-full bg-transparent"
             >
               <svg
                 className="w-10 h-10 fill-current mr-5"
@@ -336,6 +351,31 @@ export default function QuestionPage() {
                 </g>
               </svg>
               Next Question
+            </a>
+          </div>
+        ) : (
+          <div className="mt-10 rounded-xl bg-gradient-to-r from-gray-400 to-gray-600 text-gray-800">
+            <a
+              href={`#/question/${nextQuestion}`}
+              className="p-3 flex items-center rounded-xl text-2xl font-medium justify-center w-full bg-transparent"
+            >
+              <svg
+                className="w-10 h-10 fill-current mr-5"
+                viewBox="0 0 16 16"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+              >
+                <g>
+                  <path d="M8.245 4.695a.75.75 0 00-.05 1.06l1.36 1.495H4.75a.75.75 0 000 1.5h4.805l-1.36 1.495a.75.75 0 001.11 1.01l2.5-2.75a.75.75 0 000-1.01l-2.5-2.75a.75.75 0 00-1.06-.05z" />
+
+                  <path
+                    fill-rule="evenodd"
+                    d="M0 8a8 8 0 1116 0A8 8 0 010 8zm8-6.5a6.5 6.5 0 100 13 6.5 6.5 0 000-13z"
+                    clip-rule="evenodd"
+                  />
+                </g>
+              </svg>
+              Questions are completed
             </a>
           </div>
         )}
